7ce8b3af8ed113a5746a7c0a8149b8ec5b8c52a9
shadowanimexyz <shadowanimexyz@gmail.com>
2026-05-21 00:18:37 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,110 @@
+import { auth }
+from "./firebase-config.js";
+
+import {
+    onAuthStateChanged,
+    signOut
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+onAuthStateChanged(auth, user => {
+
+    if(!user){
+        window.location.href = "login.html";
+        return;
+    }
+
+    profileEmail.innerText = user.email;
+
+    // LOAD SAVED PROFILE
+
+    let savedName =
+    localStorage.getItem("profileName");
+
+    let savedBio =
+    localStorage.getItem("profileBio");
+
+    let savedAvatar =
+    localStorage.getItem("profileAvatar");
+
+    if(savedName){
+        profileName.value = savedName;
+    }
+
+    if(savedBio){
+        profileBio.value = savedBio;
+    }
+
+    if(savedAvatar){
+        profileAvatar.src = savedAvatar;
+    }
+
+    // STATS
+
+    let watchlist =
+    JSON.parse(localStorage.getItem("watchlist")) || [];
+
+    let animeData =
+    JSON.parse(localStorage.getItem("animeData")) || [];
+
+    let movieData =
+    JSON.parse(localStorage.getItem("movieData")) || [];
+
+    watchlistCount.innerText = watchlist.length;
+    animeCount.innerText = animeData.length;
+    movieCount.innerText = movieData.length;
+});
+
+// SAVE PROFILE
+
+saveProfileBtn.onclick = () => {
+
+    localStorage.setItem(
+        "profileName",
+        profileName.value
+    );
+
+    localStorage.setItem(
+        "profileBio",
+        profileBio.value
+    );
+
+    alert("Profile Saved!");
+};
+
+// CHANGE PROFILE PHOTO
+
+avatarInput.addEventListener("change", () => {
+
+    const file = avatarInput.files[0];
+
+    if(file){
+
+        const reader = new FileReader();
+
+        reader.onload = e => {
+
+            profileAvatar.src = e.target.result;
+
+            localStorage.setItem(
+                "profileAvatar",
+                e.target.result
+            );
+        };
+
+        reader.readAsDataURL(file);
+    }
+});
+
+// LOGOUT
+
+logoutBtn.onclick = () => {
+
+    signOut(auth).then(() => {
+
+        window.location.href =
+        "login.html";
+
+    });
+
+};
